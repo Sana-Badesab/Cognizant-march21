321cff8de2b8662d088cb5545da64586ce31bf93
Sana-Badesab <sanabadesab@gmail.com>
2021-03-18 17:56:52 +0530

    LAB1

---
+++ DifferenceCalculation.java
@@ -0,0 +1,27 @@
+/**
+ * Name:Sana
+ * Description:A program to calculate difference between sum of squares and square of sums
+ *
+ */
+import java.util.Scanner;
+import java.lang.Math;
+public class DifferenceCalculation {
+
+	public static void main(String[] args) {
+		      Scanner sc=new Scanner(System.in);
+				
+				System.out.println("Enter the number");
+				int range=sc.nextInt();
+		        System.out.println("The difference is:"+DifferenceCalculation(range));
+			}
+		    private static int DifferenceCalculation(int n)
+		    {
+		    	int sum=0;
+		    	int sumOfN=0,sumOfSquares=0;
+		    	sumOfN=(n*(n+1)/2);
+		    	sumOfSquares=((n*(n+1)*(2*n+1))/6);
+		    	sum=(sumOfN*sumOfN)-sumOfSquares;
+		    	return sum;
+	}
+
+}
