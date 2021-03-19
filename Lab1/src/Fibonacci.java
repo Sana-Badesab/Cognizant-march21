9f69cafbb6704e47426d50f5f78279d09d0ace19
Sana-Badesab <sanabadesab@gmail.com>
2021-03-19 12:45:54 +0530

    LAB1

---
+++ Lab1/src/Fibonacci.java
@@ -0,0 +1,21 @@
+/*
+ * Name:Sana
+ * Description:A program that prints fibonacci numbers till the given n value
+ */
+
+import java.util.*;
+public class Fibonacci {
+    public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int count=sc.nextInt();
+	int n1=0,n2=1,n3,i;
+	System.out.println(n1+"\n"+n2);
+	for(i=2;i<count;i++)
+	{
+		n3=n1+n2;
+		System.out.println(" "+n3);
+		n1=n2;
+		n2=n3;
+	}
+ }
+}
---
+++ Lab1/src/Trafficlights.java
@@ -0,0 +1,25 @@
+/*
+ * Name:Sana
+ * Description:A program that displays the traffic signal message based on the input from the user
+ */
+import java.util.Scanner;
+public class Trafficlights {
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int Signal=sc.nextInt();
+		switch(Signal)
+		{
+		
+			case 1:System.out.println("Red:Stop");
+			break;
+			case 2:System.out.println("Yellow:Ready");
+			break;
+			case 3:System.out.println("Green:Go");
+			break;
+			default:System.out.println("Not a valid input,Enter numbers from 1 to 3");
+		}
+		
+	}
+
+}
